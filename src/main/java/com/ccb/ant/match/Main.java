b7bd1e0e2610f64d23cc080f30d52378c95f792a
Goodmen <841306289@qq.com>
2024-07-01 15:25:19 +0800

    format netty-http

--- src/main/java/com/ccb/ant/match/Main.java
+++ src/main/java/com/ccb/ant/match/Main.java
@@ -2,12 +2,19 @@ package com.ccb.ant.match;
 
 import org.springframework.util.AntPathMatcher;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         AntPathMatcher matcher = new AntPathMatcher();
         String path = "/?productName=apigatewaynext#/apis/list**";
         String url = "https://one.console.tbres.telkomsigma.co.id/?productName=apigatewaynext#/apis/list?searchBy=ApiName";
         System.out.println(matcher.match("**" + path, url));
+
+        List<String> name = Arrays.asList("aaa", "Bbb");
+        System.out.println(String.join(",", name));
+
     }
 
 
---
+++ src/main/java/com/ccb/cache/Main.java
@@ -0,0 +1,58 @@
+package com.ccb.cache;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private final static Cache<String, String> cache = CacheBuilder.newBuilder()
+            .expireAfterWrite(20, TimeUnit.SECONDS)
+            .removalListener((notification) -> System.out.println("移除掉的缓存值" +   //移除缓存的回调方法
+            notification.getCause().name() + ":" +
+            notification.getKey() + ":" +
+            notification.getValue()))
+            .build();
+
+    private static void init() {
+        cache.put("IDR", "IDR_INFO");
+        cache.put("ZAR", "ZAR_INFO");
+        cache.put("DZD", "DZD_INFO");
+    }
+
+    /**
+     * 缓存过期，size！=0，但asMap是空
+     *  因为size是一个近似值，asMap()视图可以遍历缓存中的每个元素，因此可以跳过等待清理的无效条目。
+     * @param args
+     */
+    public static void main(String[] args) {
+        Logger logger = Logger.getAnonymousLogger();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if ("init".equals(input)) {
+                init();
+                logger.info("init successful");
+            } else if (input.startsWith("get")) {
+                String[] arr = input.split(":");
+                String key = arr[1];
+                String value = cache.getIfPresent(key);
+                logger.info(value);
+            } else if (input.startsWith("put")) {
+                String[] arr = input.split(":");
+                String key = arr[1];
+                String value = arr[2];
+                cache.put(key, value);
+            } else if (input.startsWith("all")) {
+                logger.info("all:" + cache.asMap().keySet());
+                logger.info("all:" + cache.asMap().values());
+            } else if (input.startsWith("size")) {
+                logger.info(" size: " + cache.size());
+            }
+        }
+    }
+}
